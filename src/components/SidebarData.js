f74dc99851c59febdb85b4b5cb04bad05b39bcee
Cristian <cristellez.694@gmail.com>
2021-05-31 16:59:01 -0500

    version final del codigo

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -4,40 +4,40 @@ import * as AiIcons from 'react-icons/ai';
 import * as IoIcons from 'react-icons/io';
 
 export const SidebarData = [
-    {
-        tittle: 'Home',
-        path: '/',
-        icon: <AiIcons.AiFillHome/>,
-        cName: 'nav-text'
-    },
-    {
-        tittle: 'Reports',
-        path: '/reports',
-        icon: <IoIcons.IoIosPaper/>,
-        cName: 'nav-text'
-    },
-    {
-        tittle: 'Products',
-        path: '/products',
-        icon: <FaIcons.FaCartPlus/>,
-        cName: 'nav-text'
-    },
-    {
-        tittle: 'Team',
-        path: '/',
-        icon: <IoIcons.IoMdPeople/>,
-        cName: 'nav-text'
-    },
-    {
-        tittle: 'Messages',
-        path: '/messages',
-        icon: <FaIcons.FaEnvelopeOpenText/>,
-        cName: 'nav-text'
-    },
-    {
-        tittle: 'Support',
-        path: '/support',
-        icon: <IoIcons.IoMdHelpCircle/>,
-        cName: 'nav-text'
-    },    
-]
+  {
+    title: 'Home',
+    path: '/',
+    icon: <AiIcons.AiFillHome />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Reports',
+    path: '/reports',
+    icon: <IoIcons.IoIosPaper />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Products',
+    path: '/products',
+    icon: <FaIcons.FaCartPlus />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Team',
+    path: '/team',
+    icon: <IoIcons.IoMdPeople />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Messages',
+    path: '/messages',
+    icon: <FaIcons.FaEnvelopeOpenText />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Support',
+    path: '/support',
+    icon: <IoIcons.IoMdHelpCircle />,
+    cName: 'nav-text'
+  }
+];
